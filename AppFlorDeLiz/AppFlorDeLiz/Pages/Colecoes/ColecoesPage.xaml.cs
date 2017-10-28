9f23d0172f62f7f76781521923adc8e8b5e351ae
Deric Mateus de Oliveira Martins <deric_mateus10@hotmail.com>
2017-10-28 10:42:04 -0300

    Adicionar arquivos de projeto.

---
+++ AppFlorDeLiz/AppFlorDeLiz/Pages/Colecoes/ColecoesPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppFlorDeLiz.Pages.Colecoes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ColecoesPage : ContentPage
+    {
+        public ColecoesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppFlorDeLiz/AppFlorDeLiz/Pages/MenuPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using AppFlorDeLiz.Pages.Colecoes;
+using AppFlorDeLiz.Pages.Sapatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppFlorDeLiz.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPage : ContentPage
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ColecoesOnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ColecoesPage());
+        }
+
+        private async void SapatosOnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SapatosPage());
+        }
+    }
+}
